edefd94f458c4a262a6cd1f7c4b2bd195c2729a0
Nikita Kolesnikov <nikolesnikov100@gmail.com>
2025-02-25 08:56:41 +0200

    PC13 LED Output

--- main.c
+++ main.c
@@ -2,6 +2,17 @@
 #include <stm32f10x.h>
 #include <stdbool.h>
 
-int __attribute((noreturn)) main(void) {
+int main(void) {
+    // int i = 0;
+    // int mask = 8; // 8 = 0b10000 = 0x8 = (1 << 4)
+    // i = i | mask; // i |= mask;
 
+
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN; // 0b10000=0x10
+    GPIOC->CRH &= ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13); // GPIOC->CRH[23:20]=0000
+    GPIOC->CRH |= GPIO_CRH_MODE13_0; // GPIOC->CRH[23:20]=0001
+    GPIOC->ODR = GPIO_ODR_ODR13;
+
+
+return 0;
 }
