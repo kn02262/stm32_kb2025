a244bf76384dacd3a764dd476e0b14ad7d24496a
Nikita Kolesnikov <nikolesnikov100@gmail.com>
2025-02-25 11:28:23 +0200

    TIM2 Timer

--- main.c
+++ main.c
@@ -20,25 +20,51 @@ void delay_us(uint32_t us){
 }
 
 
+
+/* Interrupt handler */
+void TIM2_IRQHandler(void) {
+    if (TIM2->SR & TIM_SR_UIF) {
+        if(GPIOC->ODR & GPIO_ODR_ODR13){
+            GPIOC->ODR &= ~GPIO_ODR_ODR13;
+        } else {
+            GPIOC->ODR |= GPIO_ODR_ODR13;
+        }
+    //Clear Interrupt flag
+    TIM2->SR &= ~TIM_SR_UIF;
+    }
+}
+
+
 int main(void) {
     // int i = 0;
     // int mask = 8; // 8 = 0b10000 = 0x8 = (1 << 4)
     // i = i | mask; // i |= mask;
 
-
+    /* IO PORTS Configuration */
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN; // 0b10000=0x10
     GPIOC->CRH &= ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13); // GPIOC->CRH[23:20]=0000
     GPIOC->CRH |= GPIO_CRH_MODE13_0; // GPIOC->CRH[23:20]=0001
 
-    while(1){
+    /* TIM2 Configuration */
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+    RCC->APB1RSTR |= RCC_APB1RSTR_TIM2RST;
+    RCC->APB1RSTR &= ~RCC_APB1RSTR_TIM2RST;
+    TIM2->PSC = 1023;
+    TIM2->ARR = 4095;
+    TIM2->DIER |= TIM_DIER_UIE; // Enable Update Interrupt
+    NVIC_ClearPendingIRQ(TIM2_IRQn);
+    NVIC_EnableIRQ(TIM2_IRQn); // Enable IRQ in NVIC
+    TIM2->CR1 |= TIM_CR1_CEN; // Start timer
+    while (1) {
+        __asm volatile ("nop");
+    }
+
+    /*while(1){
         GPIOC->ODR &= ~GPIO_ODR_ODR13;
         delay_us(1000000);
         GPIOC->ODR |= GPIO_ODR_ODR13;
         delay_us(1000000);
-    }
-
-
-
+    }*/
 
 return 0;
 }
